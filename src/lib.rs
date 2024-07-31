288ea166f9b95329d05b152b7a42ae981d012aac
Shanin Roman <shanin1000@yandex.ru>
2024-07-31 12:55:20 +0300

    adjust binary sizes

--- src/lib.rs
+++ src/lib.rs
@@ -5,28 +5,35 @@ pub async fn send_recv_loop(mut stream: TcpStream, id: u8) {
         let mut interval = interval(Duration::from_millis(1));
         let (mut read, mut write) = stream.split();
 
-        let buf = [0u8; 4096];
-        let mut rbuf = [0u8; 4096];
+        // Message size caused lock 
+        let buf = [0u8; 454862];
+        let mut rbuf = [0u8; 454862];
+
+        let mut i: usize = 0;
 
         loop {
             tokio::select! {
                 _ = interval.tick() => {
+                    println!("{id}/{i}: Start writing data");
+
                     if let Err(err) = write.write_all(&buf).await {
-                        println!("{id}: Error while writing data: {err}");
+                        println!("{id}/{i}: Error while writing data: {err}");
                         break;
                     }
-                    println!("{id}: Wrote data");
+                    println!("{id}/{i}: Wrote data");
                 },
                 result = read.read(&mut rbuf) => {
                     match result {
-                        Ok(n) => println!("{id}: Read data {n} bytes"),
+                        Ok(n) => println!("{id}/{i}: Read data {n} bytes"),
                         Err(err) => {
-                            println!("{id}: Error while reading data: {err}");
+                            println!("{id}/{i}: Error while reading data: {err}");
                             break;
                         }
                     }
                 }
                 else => break,
             }
+
+            i += 1;
         }
 }
